6fa57c83e352399ac9880e31fe7bb8595f724e38
Shubham <shubham@example.com>
2026-02-18 17:05:40 +0530

    Fix settings.py BASE_DIR and add executable build.sh

--- boardgames/settings.py
+++ boardgames/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev')
